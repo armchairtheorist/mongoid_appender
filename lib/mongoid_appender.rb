025ab19e059c3f8001fdaeff179d9b9a8d25a48a
Jonathan Wong <jonathan@armchairtheorist.com>
2017-05-19 13:39:55 +0800

    rubocop

--- lib/mongoid_appender.rb
+++ lib/mongoid_appender.rb
@@ -1,50 +1,49 @@
-require "logging"
-require "mongoid"
+require 'logging'
+require 'mongoid'
 
 class MongoidAppender < Logging::Appender
+  VERSION = '0.0.3'.freeze
 
-	VERSION = "0.0.3"
+  class Log
+    include Mongoid::Document
+    include Mongoid::Timestamps::Created
 
-	class Log
-		include Mongoid::Document
-		include Mongoid::Timestamps::Created
+    store_in collection: 'logs'
 
-		store_in collection: "logs"
+    field :level,		type: String
+    field :logger,	type: String
+    field :message,	type: String
+    field :exception,	type: String
+    field :backtrace,	type: Array
 
-		field :level,		type: String
-		field :logger,		type: String
-		field :message,		type: String
-		field :exception,	type: String
-		field :backtrace,	type: Array
+    index(level: 1)
+    index(logger: 1)
+    index(exception: 1)
+  end
 
-		index({ level: 1})
-		index({ logger: 1})
-		index({ exception: 1})
-	end
+  def initialize(name, opts = {})
+    super(name, opts)
+  end
 
-	def initialize (name, opts = {})
-    	super(name, opts)
- 	end
-
-	def write (event)
-		if event.instance_of?(Logging::LogEvent)
-			if event.data.is_a?(Exception)
-				Log.create!(
-					level: Logging::LNAMES[event.level],
-					logger: event.logger,
-					message: event.data.message,
-					exception: event.data.class.to_s,
-					backtrace: event.data.backtrace
-				)
-			else
-				Log.create!(
-					level: Logging::LNAMES[event.level],
-					logger: event.logger,
-					message: event.data.to_s
-				)
-			end
-		else
-			Log.create!(message: event.to_s)
-		end
-	end
+  def write(event)
+    if event.instance_of?(Logging::LogEvent)
+      if event.data.is_a?(Exception)
+        Log.create!(
+          level: Logging::LNAMES[event.level],
+          logger: event.logger,
+          message: event.data.message,
+          exception: event.data.class.to_s,
+          backtrace: event.data.backtrace
+        )
+      else
+        Log.create!(
+          level: Logging::LNAMES[event.level],
+          logger: event.logger,
+          message: event.data.to_s
+        )
+      end
+    else
+      Log.create!(message: event.to_s)
+    end
+  end
 end
--- mongoid_appender.gemspec
+++ mongoid_appender.gemspec
@@ -1,17 +1,17 @@
-require File.expand_path("../lib/mongoid_appender", __FILE__)
+require File.expand_path('../lib/mongoid_appender', __FILE__)
 
 Gem::Specification.new do |spec|
-  spec.name          = "mongoid_appender"
+  spec.name          = 'mongoid_appender'
   spec.version       = MongoidAppender::VERSION
-  spec.authors       = ["Jonathan Wong"]
-  spec.email         = ["jonathan@armchairtheorist.com"]
+  spec.authors       = ['Jonathan Wong']
+  spec.email         = ['jonathan@armchairtheorist.com']
   spec.summary       = "Simple implementation of a logging appender for Tim Pease's Logging framework using Mongoid."
-  spec.homepage      = "https://github.com/armchairtheorist/mongoid_appender"
-  spec.license       = "MIT"
+  spec.homepage      = 'https://github.com/armchairtheorist/mongoid_appender'
+  spec.license       = 'MIT'
 
-  spec.files         = Dir["{lib}/**/*.rb"]
-  spec.require_paths = ["lib"]
+  spec.files         = Dir['{lib}/**/*.rb']
+  spec.require_paths = ['lib']
 
-  spec.add_dependency "logging", ">= 1.8.0"
-  spec.add_dependency "mongoid", ">= 3.0.0"
+  spec.add_dependency 'logging', '>= 1.8.0'
+  spec.add_dependency 'mongoid', '>= 3.0.0'
 end
